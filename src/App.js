1ebb6a23bcaefed9104a7bf3f3d5a0c1eb96560a
Aileen Alvarado <monsealvarado775@gmail.com>
2024-01-23 17:41:51 -0600

    Creating buttons functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
+import calculatorLogo from './images/calculator-logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='calculator-logo-container'>
+        <img
+          src={calculatorLogo}
+          className='calculator-logo'
+          alt='calculator logo' />
+      </div> 
+      <div className='calculator-container'>  
+      <div className='row'>
+        <Button>1</Button>
+        </div> 
+      <div className='row'></div>
+      <div className='row'></div>
+      <div className='row'></div> 
+      <div className='row'></div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+
+function Button(props){
+  const  isOperator = value => {
+    return isNaN(value) && (value != '.') && (value != '=');
+  };
+    return ( 
+        <div 
+        className= {` container-button ${isOperator(props.children ? 'operator' : null)}`} >
+          {props.children}
+        </div>
+    );
+}
+
+export default Button;
